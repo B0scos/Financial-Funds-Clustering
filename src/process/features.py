b81898597c885eb5451af6fa964b139d3937c305
B0scos <lol.lol.lol.matheus@gmail.com>
2025-12-25 21:07:52 -0300

    Basic features

---
+++ src/process/features.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Iterable, List, Optional, Dict, Any
+import pandas as pd
+
+from src.utils.custom_logger import get_logger
+from src.utils.custom_exception import CustomException
+
+
+logger = get_logger(__name__)
+
+
+class FeaturesCreation:
+
+    def __init__(self, df : pd.DataFrame):
+        self.df = df
+        # certifies that the df is sorted by date
+        self.df = self.df.sort_values('report_date')
+
+        # creates a groupby cnpj
+        self.group_by_cnpj = self.df.groupby('fund_cnpj')
+
+    
+    def _add_return(self):
+        logger.info("Creating 'return' feature")
+        self.df['return'] = self.group_by_cnpj['quota_value'].pct_change()
+        return self.df
+    
+    def _add_gross_by_net(self):
+        logger.info("Creating 'gross_by_net' feature")
+        self.df['gross_by_net'] = self.df['total_value'] / self.df['net_asset_value']
+        return self.df
+    
+    def _add_feature3(self):
+        logger.info("Creating '' feature")
