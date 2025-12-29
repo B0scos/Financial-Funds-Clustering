dd7efed64d9f872993e5cfc33e52b8e216f05ff7
B0scos <lol.lol.lol.matheus@gmail.com>
2025-12-29 07:27:23 -0300

    PCA and RobustScaller

--- src/pipelines/train_pipeline.py
+++ src/pipelines/train_pipeline.py
@@ -1,16 +1,20 @@
-from sklearn.cluster import KMeans
+from src.config.settings import DATA_TRAIN_PATH_WITH_FEATURES, DATA_TEST_PATH_WITH_FEATURES, DATA_VALIDATION_PATH_WITH_FEATURES
 import pandas as pd
-from src.utils.custom_logger import get_logger
+from src.model.model import PCAWrapper, RobustScalerWrapper
 
-logger = get_logger(__name__)
+def preprocessing(n_components):
+    df_train = pd.read_parquet(DATA_TRAIN_PATH_WITH_FEATURES)
+    df_test = pd.read_parquet(DATA_TEST_PATH_WITH_FEATURES)
+    df_val = pd.read_parquet(DATA_VALIDATION_PATH_WITH_FEATURES)
+
+    robust_scaler = RobustScalerWrapper(df_train, df_test, df_val)
+
+    train_scalled, test_scalled, val_scalled = robust_scaler.fit_transform()
 
 
-def train_pipeline(train_df : pd.DataFrame, test_df : pd.DataFrame, model):
+    pca = PCAWrapper(train_scalled, test_scalled, val_scalled, n_components=n_components)
+    
 
-    logger.info("Starting the train_pipeline")
+    train_pca, test_pca, val_pca = pca.fit_transform()
 
-    model = KMeans()
-        
-    train_clusters = model.fit_predict(train_df)
-    test_clusters = model.predict(test_df)
-
+    print(train_pca, test_pca, val_pca)
